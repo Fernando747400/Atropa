1d67c855d2222256d10d152ae6f8d0df2afbe0f2
Fernando747 <fernandojcr5@gmail.com>
2021-12-03 16:27:06 -0600

    Final Animation

--- Atropa/Assets/Fer/_Code/Code_Collectibles/AliceBeverage.cs
+++ Atropa/Assets/Fer/_Code/Code_Collectibles/AliceBeverage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Rendering.PostProcessing;
+using com.amerike.Fernando;
 
 public class AliceBeverage : MonoBehaviour
 {
@@ -10,6 +11,13 @@ public class AliceBeverage : MonoBehaviour
     public PostProcessVolume postProcess;
     private Vignette vignette;
     private bool ending;
+    public Animator endingAnimation;
+    public GameObject mainCamera;
+    public GameObject head;
+    public GameObject bearUp;
+    public GameObject bearDown;
+    public PlayerCamera playerCamera;
+    public PlayerMovement playerMovement;
 
     public void Start()
     {
@@ -20,6 +28,7 @@ public class AliceBeverage : MonoBehaviour
 
     public void drinkAlice()
     {
+        endAnim();
         StartCoroutine(endSequence());
     }
 
@@ -40,5 +49,17 @@ public class AliceBeverage : MonoBehaviour
         {
             vignette.intensity.value = Mathf.Lerp(vignette.intensity.value, 1, Time.deltaTime/9f);
         }
+    }
+
+    public void endAnim()
+    {
+        playerCamera.Active = false;
+        playerMovement.Active = false;
+        iTween.MoveTo(mainCamera,head.transform.position,0.2f);
+        mainCamera.transform.rotation = head.transform.rotation;
+        mainCamera.transform.parent = head.transform;
+        bearUp.SetActive(false);
+        bearDown.SetActive(true);
+        endingAnimation.Play("DyingAnimation");
     }
 }
